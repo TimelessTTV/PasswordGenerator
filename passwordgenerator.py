01146a86b97193e1786ae9198dde22c71b94dd4e
TimelessTTV <85189523+TimelessTTV@users.noreply.github.com>
2022-07-19 02:24:34 +0300

    Add passwordgenerator.py

---
+++ passwordgenerator.py
@@ -0,0 +1,23 @@
+import random
+import string
+import asyncio
+import os
+
+print("Password Generator is get up and ready to go.")
+
+length = int(input('\nEnter password length:'))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+temp = random.sample(all,length)
+
+password = "".join(temp)
+
+print(password)
+
+os.system("pause")
